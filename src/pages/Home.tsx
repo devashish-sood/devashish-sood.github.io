932eb4492f037e01276d5b064f0785fcb98d9553
Devashish Sood <39184335+devashish-sood@users.noreply.github.com>
2026-05-16 10:46:47 -0700

    a

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,13 +12,13 @@ const Home = () => {
       <div className="flex row">
         <img src="/mountains.jpg" alt="Mountains" className="hidden md:block" style={{ width: "40%" }} />
         <div className="md:pl-10">
-          Currently working on model interpretability.
+          <Link href="https://github.com/p3rciv3l/winnie-the-pooh-qwen" text="Currently working on model interpretability." />
         </div>
       </div>
       <br />
       <p>
         More information on my projects and work experiences can be found on my{" "}
-        <Link href="https://drive.google.com/file/d/11TIYQ8hPVEQqvS0HKtSGVf_Ln6lftZUM/view?usp=sharing" text="resume" /> and{" "}
+        <Link href="https://drive.google.com/file/d/18WjBBl4938KS-A6ha6iBK-K66BJrh141/view?usp=sharing" text="resume" /> and{" "}
         <Link href="https://github.com/devashish-sood" text="github" />. I can
         also be contacted via{" "}
         <Link href="https://linkedin.com/in/devashishsood9" text="linkedin" />{" "}
